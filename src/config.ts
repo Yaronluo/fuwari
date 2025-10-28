66a1fb8bed17821ebbf4bb3689c406759c5796d4
Yaronluo <luoyangrun@gmail.com>
2025-10-28 14:57:16 +0800

    更新博客配置

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "YaronBlog",
-	subtitle: "Demo Site",
+	subtitle: "Yaron Life",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -54,26 +54,36 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	name: "Yaron",
+	bio: "日落尤其温暖，人间皆是热爱",
 	links: [
-		{
-			name: "Twitter",
-			icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
+		//{
+			//name: "Twitter",
+			//icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
 			// You will need to install the corresponding icon set if it's not already included
 			// `pnpm add @iconify-json/<icon-set-name>`
-			url: "https://twitter.com",
-		},
-		{
-			name: "Steam",
-			icon: "fa6-brands:steam",
-			url: "https://store.steampowered.com",
-		},
+			//url: "https://twitter.com",
+		//},
+		//{
+			//name: "Steam",
+			//icon: "fa6-brands:steam",
+			//url: "https://store.steampowered.com",
+		//},
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
-			url: "https://github.com/saicaca/fuwari",
+			url: "https://github.com/Yaronluo",
 		},
+        {
+            name: 'QQ',
+            icon: 'fa6-brands:qq',
+            url: 'https://qm.qq.com/q/2jKJhKcCjm',
+        },
+        {
+            name: 'Email',
+            icon: 'fa6-solid:envelope',
+            url: 'mailto:2575586108@qq.com',
+        },
 	],
 };
 
