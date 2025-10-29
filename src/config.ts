1ee2aff87ebea02e63ee28a8e42ba8df027f62fb
Yaronluo <luoyangrun@gmail.com>
2025-10-29 09:45:51 +0800

    更新

--- src/config.ts
+++ src/config.ts
@@ -13,7 +13,7 @@ export const siteConfig: SiteConfig = {
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 90, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-		fixed: false, // Hide the theme color picker for visitors
+		fixed: true, // Hide the theme color picker for visitors
 	},
 	banner: {
 		enable: false,
@@ -32,7 +32,7 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		// Leave this array empty to use the default favicon
 		 {
-		   src: 'public/favicon/icon1.png', // Path of the favicon, relative to the /public directory
+		   src: 'public/favicon/favicon.ico', // Path of the favicon, relative to the /public directory
 		   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
 		   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
 		 }
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/avatar1.jpeg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/avatar2.webp", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Yaron",
 	bio: "日落尤其温暖，人间皆是热爱",
 	links: [
